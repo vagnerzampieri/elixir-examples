500469c53da7ac3844213f51d601801ee2b4e6e8
Vagner Zampieri <vgzampieri@gmail.com>
2019-08-08 21:17:13 -0300

    Make some test for Calculator

--- part-2/calculator/test/calculator_test.exs
+++ part-2/calculator/test/calculator_test.exs
@@ -9,4 +9,18 @@ defmodule CalculatorTest do
   test "returns value added", context do
     assert Calculator.add(context[:server_id], 10) == 10
   end
+
+  test "returns value subtracted", context do
+    assert Calculator.sub(context[:server_id], 5) == -5
+  end
+
+  test "returns value multiplied", context do
+    Calculator.add(context[:server_id], 10)
+    assert Calculator.mult(context[:server_id], 10) == 100
+  end
+
+  test "returns value divided", context do
+    Calculator.add(context[:server_id], 10)
+    assert Calculator.div(context[:server_id], 10) == 1
+  end
 end
