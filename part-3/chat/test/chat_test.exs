1bcffa3a4b0a1d663c1e2c1bbc4050df99f1aaea
Vagner Zampieri <vgzampieri@gmail.com>
2019-08-10 15:51:50 -0300

    Add test for Chat

--- part-3/chat/test/chat_test.exs
+++ part-3/chat/test/chat_test.exs
@@ -1,8 +1,11 @@
 defmodule ChatTest do
   use ExUnit.Case
-  doctest Chat
+  doctest Chat.Server
 
-  test "greets the world" do
-    assert Chat.hello() == :world
+  describe "#add_msg" do
+    test "returns added menssage" do
+      Chat.Server.add_msg "Hello"
+      assert Chat.Server.get_msgs == ["Hello"]
+    end
   end
 end
