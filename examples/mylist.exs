36bb93ad1a4f5e05c0f19fcfaab7e45ad5bfd29b
Vagner Zampieri <vgzampieri@gmail.com>
2019-08-17 10:21:11 -0300

    Add sum to Mylist

--- examples/mylist.exs
+++ examples/mylist.exs
@@ -2,6 +2,7 @@
 # Mylist.square([1,2,3,4])
 # Mylist.map([1,2,3], fn(n) -> n*n end)
 # Mylist.map([1,2,3], &(&1*&1))
+# Mylist.sum([1,2,3])
 defmodule Mylist do
     def len(nil), do: 0
     def len([]), do: 0
@@ -14,4 +15,8 @@ defmodule Mylist do
     def map(nil), do: []
     def map([], _func), do: []
     def map([head | tail], func), do: [func.(head) | map(tail, func)]
+
+    def sum(list), do: _sum(list, 0)
+    defp _sum([], total), do: total
+    defp _sum([head | tail], total), do: _sum(tail, head + total) 
 end
