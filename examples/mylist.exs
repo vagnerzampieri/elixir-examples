44e4877f72defb11438993540b828eee4fc46255
Vagner Zampieri <vgzampieri@gmail.com>
2019-08-16 22:32:47 -0300

    Add map with anonymous function

--- examples/mylist.exs
+++ examples/mylist.exs
@@ -1,5 +1,7 @@
 # Mylist.len([1,2,3,4])
 # Mylist.square([1,2,3,4])
+# Mylist.map([1,2,3], fn(n) -> n*n end)
+# Mylist.map([1,2,3], &(&1*&1))
 defmodule Mylist do
     def len(nil), do: 0
     def len([]), do: 0
@@ -8,4 +10,8 @@ defmodule Mylist do
     def square(nil), do: []
     def square([]), do: []
     def square([head | tail]), do: [head*head | square(tail)]
+
+    def map(nil), do: []
+    def map([], _func), do: []
+    def map([head | tail], func), do: [func.(head) | map(tail, func)]
 end
