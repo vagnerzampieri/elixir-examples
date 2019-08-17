763c6a7e41d05c926b53ba285f95c147b174ed5b
Vagner Zampieri <vgzampieri@gmail.com>
2019-08-16 22:16:12 -0300

    Add square recurssion

--- examples/mylist.exs
+++ examples/mylist.exs
@@ -1,6 +1,11 @@
 # Mylist.len([1,2,3,4])
+# Mylist.square([1,2,3,4])
 defmodule Mylist do
     def len(nil), do: 0
     def len([]), do: 0
     def len([_head | tail]), do: 1 + len(tail)
+
+    def square(nil), do: []
+    def square([]), do: []
+    def square([head | tail]), do: [head*head | square(tail)]
 end
